66105d0f8ee393edf001f444227d8eeb007a1ec3
Kykypy3a1 <108570735+Kykypy3a1@users.noreply.github.com>
2022-07-05 10:25:17 +0300

    Пэсня

--- untitled/src/helloWorld.java
+++ untitled/src/helloWorld.java
@@ -1,5 +1,7 @@
 public class helloWorld {
     public static void main(String[] args) {
         System.out.println("Привет мир");
+        System.out.println("My name is.. (what?) My name is.. (who?) ");
+        System.out.println("My name is.. {scratches} Slim Shady");
     }
 }
