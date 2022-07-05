e7762914055ac548028bdf11891f3924b89f70ba
Kykypy3a1 <108570735+Kykypy3a1@users.noreply.github.com>
2022-07-05 10:29:50 +0300

    так надо

--- untitled/src/helloWorld.java
+++
@@ -1,7 +0,0 @@
-public class helloWorld {
-    public static void main(String[] args) {
-        System.out.println("Привет мир");
-        System.out.println("My name is.. (what?) My name is.. (who?) ");
-        System.out.println("My name is.. {scratches} Slim Shady");
-    }
-}
